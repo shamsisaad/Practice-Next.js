b18311344aa5da27cf3ca38e3c897585ee202fca
shamsisaad <engrsaadshamsi@gmail.com>
2023-08-06 12:10:07 +0500

    Add menu children

--- practice-ecommerce/components/header/index.jsx
+++ practice-ecommerce/components/header/index.jsx
@@ -14,14 +14,48 @@ export default function Header () {
                 {
                     label: 'Mens',
                     key: 'mens',
+                    children: [
+                        {
+                            label: "Men's Shirts",
+                            Key: "mens-shirts",
+                        },
+                        {
+                            label: "Men's Shoes",
+                            Key: "mens-shoes",
+                        },
+                        {
+                            label: "Men's Watches",
+                            Key: "mens-watches",
+                        },
+                    ],
                     },
                 {
                     label: 'Womens',
                     key: 'womens',
+                    children: [
+                        {
+                            label: "Women's Dresses",
+                            Key: "womens-dresses",
+                        },
+                        {
+                            label: "Women's Shoes",
+                            Key: "womens-shoes",
+                        },
+                        {
+                            label: "Women's Watches",
+                            Key: "womens-watches",
+                        },
+                    ],
                     },
                     {
                         label: 'Accessories',
                         key: 'accessories',
+                        children: [
+                            {
+                                label: "Fragrances",
+                                key: "fragrance",
+                            },
+                        ],
                         },    
 
              ]}/>
