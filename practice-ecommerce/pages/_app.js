fe3d380a02859404fbfc712c0804c12592b3e892
shamsisaad <engrsaadshamsi@gmail.com>
2023-08-06 11:07:44 +0500

    updated index.js

--- practice-ecommerce/pages/_app.js
+++ practice-ecommerce/pages/_app.js
@@ -1,8 +1,8 @@
 import '@/styles/globals.css'
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
+// import type { AppProps } from "next/app";
 import Layout from "components/Layout";
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />;
